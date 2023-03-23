5b52076fbd62a1413bfdf14ff7562e0a0b38a9b3
Luke Arnould <luke.arnould@okstate.edu>
2023-03-23 12:48:58 -0500

    Initial commit

---
+++ LABasicAppDocker/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LABasicAppDocker.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string text = "hello world!";
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var number = 0;
+            var list = new List<string>();
+            
+        }
+    }
+}
